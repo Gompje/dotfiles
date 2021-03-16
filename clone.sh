85e1ece635126500fd3b573662089a32ed980182
Veerle Deschepper <veerle@daktadeo.be>
2021-03-16 23:38:42 +0100

    update apps + git provider

--- clone.sh
+++ clone.sh
@@ -9,22 +9,10 @@ DEVIL=$SITES
 # git clone git@gitlab.com/cytopia/devilbox $SITES/devilbox
 # cp ./config/devilbox.env $SITES/devilbox/.env
 
-# Personal
-git clone git@gitlab.com:daktadeo/blogs/gompje-be.git $DEVIL/gompje-be
-
+# 
+git clone git@github.com:DaktaDeo/gompje.be.git $DEVIL/gompje-be
+git clone git@github.com:DaktaDeo/daktadeo.be.git $DEVIL/daktadeo-be 
+git clone git@github.com:DaktaDeo/bmc-flanders-offshore-racing.git $DEVIL/bmc-for
 
 # Multipass
-git clone git@gitlab.com:daktadeo/multipass/multipass-user-docs.git $DEVIL/multipass-user-docs
-git clone git@gitlab.com:daktadeo/multipass/multipass-handbook.git $DEVIL/multipass-handbook
-git clone git@gitlab.com:daktadeo/multipass/multipass-auth.git $DEVIL/multipass-auth
-git clone git@gitlab.com:daktadeo/multipass/multipass-rocks.git $DEVIL/multipass-rocks
-git clone git@gitlab.com:daktadeo/multipass/handles.git $DEVIL/handles
-
-#dockers
-git clone git@github.com:Gompje/docker-nginx-pagespeed.git $DEVIL/docker-nginx-pagespeed
-
-#DaktaDeo
-git clone git@gitlab.com:daktadeo/self/daktadeo-nuxt.git $DEVIL/daktadeo-nuxt
-git clone git@gitlab.com:daktadeo/self/daktadeo-handbook.git $DEVIL/daktadeo-handbook
-
-git clone git@github.com:DaktaDeo/bmc-flanders-offshore-racing.git
+git clone git@github.com:DaktaDeo/multipass-rocks.git $DEVIL/multipass-rocks
